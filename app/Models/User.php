d4ebba2a8ba3e124b0fb75260d9dd9041e560677
Antonrom00 <anton.romanyuk.2000@gmail.com>
2021-03-23 22:34:16 +0200

    Fix jwt auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,14 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+/**
+ * @property int id
+ * @property string name
+ * @property string email
+ */
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -25,4 +31,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier(): int
+    {
+        return $this->id;
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
 }
